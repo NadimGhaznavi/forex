ad9de77c691624f8bffb8229ff4873fbc734879f
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2025-04-29 19:34:29 -0400

    Lesson 8

---
+++ defs.py
@@ -0,0 +1,8 @@
+API_KEY = "8555e21439aaa3c511abd2e32c71f9b2-234f394810c3277e25354d91bd8ae989"
+ACCOUNT_ID = "101-002-30861672-001"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+
+}
